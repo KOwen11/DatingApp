b169b8573aa563a4402a161881a284643fda8fdf
KevinOwen <kevinowen11@gmail.com>
2020-05-26 15:33:07 -0500

    Lesson 3 complete

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -29,6 +29,7 @@ namespace DatingApp.API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // Lesson3 complete
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
